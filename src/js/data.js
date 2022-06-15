03277998be8e7181f18701b858d332beccf3ff9a
FourthStrongest <fourthstrongest@gmail.com>
2022-06-14 23:41:14 -0700

    Force new file

--- src/js/data.js
+++ src/js/data.js
@@ -15,7 +15,7 @@ const dataSet = {};
  * 
  * @example '2018-02-20'
 */
-let dataSetVersion = '';
+let dataSetVersion = '2022-06-14';
 
 /**
  * Image root, will be appended to the start of every image URL.
